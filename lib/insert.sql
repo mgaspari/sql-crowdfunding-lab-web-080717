d476277ae20a87010afb2d8577af9ffe01814c84
mgaspari <marcoanthonygaspari@gmail.com>
2017-08-15 16:40:07 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,123 @@
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+INSERT INTO users (name, age)
+    VALUES ("Zak", 12);
+
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+values ("Rocketship", "Science", 1000000000, "1/1/17", "2/1/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+values ("Rocketship", "Science", 1000000000, "1/1/17", "2/1/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+values ("Rocketship", "Science", 1000000000, "1/1/17", "2/1/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+values ("Rocketship", "Science", 1000000000, "1/1/17", "2/1/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+values ("Rocketship", "Science", 1000000000, "1/1/17", "2/1/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+values ("Rocketship", "Science", 1000000000, "1/1/17", "2/1/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+values ("Rocketship", "Science", 1000000000, "1/1/17", "2/1/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+values ("Rocketship", "Science", 1000000000, "1/1/17", "2/1/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+values ("Rocketship", "Science", 1000000000, "1/1/17", "2/1/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+values ("Rocketship", "Science", 1000000000, "1/1/17", "2/1/17");
+
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+values (678549, 2, 4);
